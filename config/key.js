ed0245cabc4d6488223b60c03e2de632cd6c99df
ThanhMumeSoft <mume.tuananhdinh@gmail.com>
2022-10-15 09:54:38 +0700

    fivestar

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://tuananh:anhvaanh@musicfivestar.qi2krpy.mongodb.net/?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const db = require("./config/key").mongoURI;
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected...."))
+    .catch((err) => console.log(err));
+app.use("/", require("./router/User"));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, console.log(`Server Run With Port ${PORT}`));
